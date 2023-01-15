2014fd57e2f4fd1915df437616098137018e8c9d
Artus Rocha <artusrocha@gmail.com>
2023-01-15 16:03:12 -0500

    initial commit

---
+++ 1/src/main/java/com/example/retries/RetiresApplication.java
@@ -0,0 +1,13 @@
+package com.example.retries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetiresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetiresApplication.class, args);
+	}
+
+}
